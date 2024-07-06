a3a264df2ae9ad4bf4900030fa20511563c1c1da
varuntyagiG <varungauravtyagi@gmail.com>
2024-07-06 15:57:39 +0530

    talwind card

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     <>
       <Card amountinfo="Amount Pending" rupee="92,312.20" orders={13} />
       <Card amountinfo="Amount Processed" rupee="23,92,312.19" orders={7} />
+      <Card amountinfo="Amount Processed" rupee="77,23445.19" orders={9} />
     </>
   );
 }
